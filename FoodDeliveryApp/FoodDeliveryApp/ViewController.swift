8f11e7e4e7fcad30f758c24ac783bf66b98f186f
YerassylOM <erasharapov01@gmail.com>
2025-03-22 15:37:34 +0500

    Created base project, removed storyboard references

---
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Yerassyl on 22.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.red
+    }
+
+
+}
+
